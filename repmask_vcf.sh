c019ae6ed931227213ef1abc64a8dee86af110ce
Cristian Groza <cristian.groza@mail.mcgill.ca>
2022-06-02 14:27:06 -0400

    Repeatmask deletions.

--- repmask_vcf.sh
+++ repmask_vcf.sh
@@ -5,7 +5,8 @@ OUT_VCF=$2
 FASTA_LIB=$3
 
 FASTA_FILE=$(mktemp)
-cat ${VCF} | grep -v "#" | awk '{print(sprintf(">%s\n%s", $3, $5))}' > ${FASTA_FILE}
+cat ${VCF} | grep -v "#" | grep "INS" | awk '{print(sprintf(">%s\n%s", $3, $5))}' > ${FASTA_FILE}
+cat ${VCF} | grep -v "#" | grep "DEL" | awk '{print(sprintf(">%s\n%s", $3, $4))}' >> ${FASTA_FILE}
 
 REPMASK_DIR=$(mktemp -d)
 
@@ -42,8 +43,7 @@ vcf_df <- tibble(CHROM = getCHROM(vcf),
 
 annot <- left_join(vcf_df, rep_mask, by = "qry_id") %>%
   replace_na(list(matching_class = "None", repeat_id = "None", match_len = 0)) %>%
-  mutate(MEI=grepl("(SINE|LINE|SVA)", matching_class),
-         match_span = match_len / qry_length) %>%
+  mutate(match_span = match_len / qry_length) %>%
   select(-c(qry_id, qry_length, match_len)) %>%
   arrange(CHROM, POS)
 
@@ -58,10 +58,9 @@ HDR_FILE=$(mktemp)
 
 echo -e '##INFO=<ID=repFamily,Number=1,Type=String,Description="Repeat family">' > ${HDR_FILE}
 echo -e '##INFO=<ID=repClass,Number=1,Type=String,Description="Repeat name">' >> ${HDR_FILE}
-echo -e '##INFO=<ID=MEI,Number=1,Type=String,Description="If repeat is SVA/L1/Alu">' >> ${HDR_FILE}
-echo -e '##INFO=<ID=match_span,Number=1,Type=Float,Description="Insertion length spaned by MEI">' >> ${HDR_FILE}
+echo -e '##INFO=<ID=match_span,Number=1,Type=Float,Description="Insertion length spaned by repeat">' >> ${HDR_FILE}
 
 bcftools annotate -a ${ANNOT_FILE}.gz -h ${HDR_FILE} \
-         -c CHROM,POS,INFO/repFamily,INFO/repClass,INFO/MEI,INFO/match_span $VCF | \
+         -c CHROM,POS,INFO/repFamily,INFO/repClass,INFO/match_span $VCF | \
     bcftools view -Oz -o ${OUT_VCF}
 
