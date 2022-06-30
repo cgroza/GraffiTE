a734ce5e9cc259f2b83462bc5dc21281a29eb835
Cristian Groza <cristian.groza@mail.mcgill.ca>
2022-06-30 12:50:19 -0400

    Correct the headers.

--- repmask_vcf.sh
+++ repmask_vcf.sh
@@ -24,11 +24,11 @@ tabix -s1 -b2 -e2 ${ANNOT_FILE}.gz
 
 HDR_FILE=$(mktemp)
 
-echo -e '##INFO=<ID=n_hits,Number=1,Type=Float,Description="Number of repeats found in insertion">' >> ${HDR_FILE}
-echo -e '##INFO=<ID=match_lengths,Number=1,Type=String,Description="Insertion lengths spanned by each repeat">' >> ${HDR_FILE}
-echo -e '##INFO=<ID=repeat_ids,Number=1,Type=String,Description="Repeat family IDs">' >> ${HDR_FILE}
-echo -e '##INFO=<ID=matching_classes,Number=1,Type=String,Description="Repeat class names">' >> ${HDR_FILE}
-echo -e '##INFO=<ID=total_match_length,Number=1,Type=Float,Description="Insertion length spanned by repeats">' >> ${HDR_FILE}
+echo -e '##INFO=<ID=n_hits,Number=1,Type=Integer,Description="Number of repeats found in insertion">' >> ${HDR_FILE}
+echo -e '##INFO=<ID=match_lengths,Number=.,Type=Integer,Description="Insertion lengths spanned by each repeat">' >> ${HDR_FILE}
+echo -e '##INFO=<ID=repeat_ids,Number=.,Type=String,Description="Repeat family IDs">' >> ${HDR_FILE}
+echo -e '##INFO=<ID=matching_classes,Number=.,Type=String,Description="Repeat class names">' >> ${HDR_FILE}
+echo -e '##INFO=<ID=total_match_length,Number=1,Type=Integer,Description="Insertion length spanned by repeats">' >> ${HDR_FILE}
 echo -e '##INFO=<ID=total_match_span,Number=1,Type=Float,Description="Insertion span spanned by repeats">' >> ${HDR_FILE}
 echo -e '##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">' >> ${HDR_FILE}
 
