a9eb5e9a5e33256318d171b8db7c38d84366e5ec
Cristian Groza <cristian.groza@mail.mcgill.ca>
2022-07-07 11:23:46 -0400

    Integrate one code to find them all in pipeline.

--- annotate_vcf.R
+++ annotate_vcf.R
@@ -69,6 +69,7 @@ rep_mask <- read_rm_custom(opt$dotout) %>%
   summarise(
     start = first(qry_start), stop = max(qry_end),
     match_lengths = paste0(match_len, collapse = ","),
+    fragmts = paste0(fragmts, collapse = ","),
     repeat_ids = paste0(repeat_id, collapse = ","),
     matching_classes = paste0(matching_class, collapse = ","),
     n_hits = n()
@@ -80,6 +81,7 @@ rep_mask <- read_rm_custom(opt$dotout) %>%
       n_hits = sum(n_hits),
       total_match_length = sum(total_match_length),
       match_lengths = paste0(match_lengths, collapse = ","),
+      fragmts = paste0(fragmts, collapse = ","),
       repeat_ids = paste0(repeat_ids, collapse = ","),
       matching_classes = paste0(matching_classes, collapse = ",")
     )
@@ -99,11 +101,12 @@ annot <- left_join(vcf_df, rep_mask, by = "qry_id") %>%
                   repeat_ids = "None",
                   n_hits = 0,
                   total_match_length = 0,
+                  fragmts = 0,
                   match_lengths = "0")) %>%
   mutate(total_match_span = total_match_length / qry_length) %>%
     select(-c(qry_id, qry_length)) %>%
     arrange(CHROM, POS) %>%
-    select(CHROM, POS, n_hits, match_lengths, repeat_ids, matching_classes, total_match_length, total_match_span)
+    select(CHROM, POS, n_hits, fragmts, match_lengths, repeat_ids, matching_classes, total_match_length, total_match_span)
 
 write_tsv(annot, file = opt$annotation, col_names = F)
 print(colnames(annot))
--- repmask_vcf.sh
+++ repmask_vcf.sh
@@ -14,10 +14,41 @@ REPMASK_DIR=repeatmasker_dir
 RepeatMasker -nolow -lib ${FASTA_LIB} -s -dir ${REPMASK_DIR} -pa 16 ${FASTA_FILE}
 
 REPMASK_OUT=${REPMASK_DIR}/$(basename ${FASTA_FILE}).out
+REPMASK_ONECODE_OUT=${REPMASK_DIR}/$(basename ${FASTA_FILE}).onecode.out
+
+### Step 1 make the dictionary file if LTRs are split between _LTR and _I
+echo "Building onecode LTR dictionary..."
+build_dictionary.pl --rm ${REPMASK_OUT} --unknown > ${REPMASK_DIR}/OneCode_LTR.dic
+
+### Step 2 run OneCodeToFindThemAll
+echo "Running onecode..."
+one_code_to_find_them_all.pl --rm ${REPMASK_OUT} --unknown --ltr ${REPMASK_DIR}/OneCode_LTR.dic > ${REPMASK_DIR}/onecode.log 2>&1
+
+### Step 3 parse the output
+# remove output if previous run
+echo "Concatenate outputs..."
+rm ${REPMASK_DIR}/ALL.elem_sorted.csv 2>/dev/null
+# loop over each individual output and concatenate in a single file (can't cat, often there are too many files)
+for i in ${REPMASK_DIR}/*.elem_sorted.csv
+do
+	cat $i >> ${REPMASK_DIR}/ALL.elem_sorted.csv
+done
+
+# parse the concatenated outputs and save the raw to recover stitched copies
+echo "Parse outputs..."
+cat <(head -n 3 ${REPMASK_OUT} | sed 's/ID/ID\tfragmts/g') <(grep '###' ${REPMASK_DIR}/ALL.elem_sorted.csv | cut -f 1-16 | sed 's/###//g') > ${REPMASK_ONECODE_OUT}
+mv ${REPMASK_DIR}/ALL.elem_sorted.csv ${REPMASK_DIR}/ALL.onecode.elem_sorted.bak
+
+# clean-up the mess (BE SURE YOU HAVE RENAMED THE OUTPUT)
+echo "Cleanup..."
+find ${REPMASK_DIR} -name "*.copynumber.csv" -delete
+find ${REPMASK_DIR} -name "*.elem_sorted.csv" -delete
+find ${REPMASK_DIR} -name "*.ltr.csv" -delete
+find ${REPMASK_DIR} -name "*.transposons.csv" -delete
 
 ANNOT_FILE=vcf_annotation
 
-annotate_vcf.R --dotout ${REPMASK_OUT} --vcf ${VCF} --annotation ${ANNOT_FILE}
+annotate_vcf.R --dotout ${REPMASK_ONECODE_OUT} --vcf ${VCF} --annotation ${ANNOT_FILE}
 
 bgzip ${ANNOT_FILE}
 tabix -s1 -b2 -e2 ${ANNOT_FILE}.gz
@@ -28,11 +59,12 @@ echo -e '##INFO=<ID=n_hits,Number=1,Type=Integer,Description="Number of repeats 
 echo -e '##INFO=<ID=match_lengths,Number=.,Type=Integer,Description="Insertion lengths spanned by each repeat">' >> ${HDR_FILE}
 echo -e '##INFO=<ID=repeat_ids,Number=.,Type=String,Description="Repeat family IDs">' >> ${HDR_FILE}
 echo -e '##INFO=<ID=matching_classes,Number=.,Type=String,Description="Repeat class names">' >> ${HDR_FILE}
+echo -e '##INFO=<ID=fragmts,Number=.,Type=Integer,Description="Number of fragments merged into one by one code">' >> ${HDR_FILE}
 echo -e '##INFO=<ID=total_match_length,Number=1,Type=Integer,Description="Insertion length spanned by repeats">' >> ${HDR_FILE}
 echo -e '##INFO=<ID=total_match_span,Number=1,Type=Float,Description="Insertion span spanned by repeats">' >> ${HDR_FILE}
 echo -e '##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">' >> ${HDR_FILE}
 
 bcftools annotate -a ${ANNOT_FILE}.gz -h ${HDR_FILE} \
-         -c CHROM,POS,INFO/n_hits,INFO/match_lengths,INFO/repeat_ids,INFO/matching_classes,INFO/total_match_length,INFO/total_match_span $VCF | \
+         -c CHROM,POS,INFO/n_hits,INFO/fragmts,INFO/match_lengths,INFO/repeat_ids,INFO/matching_classes,INFO/total_match_length,INFO/total_match_span $VCF | \
     bcftools view -Oz -o ${OUT_VCF}
 
