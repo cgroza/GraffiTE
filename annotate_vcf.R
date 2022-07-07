0458d44127b87a1137a399e8c1dcf562a61967de
Cristian Groza <cristian.groza@mail.mcgill.ca>
2022-07-07 10:08:04 -0400

    Support for custom repeat masker file.

--- annotate_vcf.R
+++ annotate_vcf.R
@@ -8,6 +8,38 @@ library(tidyr)
 library(readr)
 library(vcfR)
 
+read_rm_custom <- function(file) {
+    rm_file <- readr::read_lines(file = file, skip = 3)
+    rm_file <- lapply(rm_file, function(x) {
+        str.res <- unlist(stringr::str_split(x, "\\s+"))[-1]
+        str.res <- str.res[1:15]
+        return(str.res)
+    })
+    rm_file <- tibble::as_tibble(do.call(rbind, rm_file))
+    colnames(rm_file) <- c("sw_score", "perc_div", "perc_del",
+        "perc_insert", "qry_id", "qry_start", "qry_end", "qry_left",
+        "matching_repeat", "repeat_id", "matching_class", "no_bp_in_complement",
+        "in_repeat_start", "in_repeat_end", "fragmts")
+    qry_end <- qry_start <- NULL
+    nrow_before_filtering <- nrow(rm_file)
+    suppressWarnings(rm_file <- dplyr::mutate(rm_file,
+      qry_start = as.integer(qry_start),
+      qry_end = as.integer(qry_end), fragmts = as.integer(fragmts)
+    ))
+    rm_file <- dplyr::filter(rm_file, !is.na(qry_start), !is.na(qry_end))
+    rm_file <- dplyr::mutate(rm_file, qry_width = as.integer(qry_end -
+        qry_start + 1L))
+    nrow_after_filtering <- nrow(rm_file)
+    if ((nrow_before_filtering - nrow_after_filtering) > 0)
+        message((nrow_before_filtering - nrow_after_filtering) +
+            1, " out of ", nrow_before_filtering, " rows ~ ",
+            round(((nrow_before_filtering - nrow_after_filtering) +
+                1) / nrow_before_filtering, 3), "% were removed from the imported RepeatMasker file, ",
+            "because they contained 'NA' values in either 'qry_start' or 'qry_end'.")
+    return(rm_file)
+}
+
+
 option_list <- list(
   make_option(c("-r", "--dotout"),
     type = "character", help = "RepeatMasker annotation .out file.", metavar = "file"
@@ -29,7 +61,7 @@ if (any(is.null(opt$dotout), is.null(opt$vcf), is.null(opt$annotation))) {
   stop()
 }
 
-rep_mask <- read_rm(opt$dotout) %>%
+rep_mask <- read_rm_custom(opt$dotout) %>%
   # merge overlapping TE annotations in the same query
   mutate(match_len = qry_end - qry_start) %>%
   arrange(qry_start) %>%
