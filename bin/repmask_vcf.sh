f780169e0b1a9c180eaeeb934ac42ffb5cd92452
Clément Goubert <goubert.clement@gmail.com>
2023-08-24 12:38:38 -0400

    add missing hit_ID column

--- bin/repmask_vcf.sh
+++ bin/repmask_vcf.sh
@@ -73,7 +73,7 @@ paste -d "\t" <(echo -e "${i}") <(grep -w "${i}" merge.bed | awk '{print ($3-$2)
 done
 # merge with ${ANNOT_FILE}_1
 join -13 -21 -a1 <(sort -k3,3 ${ANNOT_FILE}_1)  <(sort -k1,1 span) | sed 's/ /\t/g' | \
- awk '{print $2"\t"$3"\t"$1"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10"\t"$11"\t"$13"\t"$15}' | \
+ awk '{print $2"\t"$3"\t"$1"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10"\t"$11"\t"$12"\t"$13"\t"$15}' | \
  awk '{if (NF == 10) {print $0"\t0\t0"} else {print $0}}' | \
  sort -k1,1 -k2,2n > ${ANNOT_FILE}
 
