ab0f38b263cdb7595a5ede9da7ba6329fe020531
Clement Goubert <goubert.clement@gmail.com>
2026-01-21 15:28:43 -0700

    add debug for annot file

--- bin/repmask_vcf.sh
+++ bin/repmask_vcf.sh
@@ -83,6 +83,7 @@ join -11 -21 \
 awk '{print $1"\t"$2"\t"$3"\t"($2/$3)}' > span
 
 # merge with ${ANNOT_FILE}_1
+join -13 -21 -a1 <(sort -k3,3 ${ANNOT_FILE}_1)  <(sort -k1,1 span) | sed 's/ /\t/g' # this is to see how it looks
 join -13 -21 -a1 <(sort -k3,3 ${ANNOT_FILE}_1)  <(sort -k1,1 span) | sed 's/ /\t/g' | \
  awk '{print $2"\t"$3"\t"$1"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10"\t"$11"\t"$12"\t"$13"\t"$15}' | \
  awk '{if (NF == 11) {print $0"\t0\t0"} else {print $0}}' | \
