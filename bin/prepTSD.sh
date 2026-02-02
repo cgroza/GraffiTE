5fa991ab0cd126295f51ef8d88360ed3fff9ea04
Clement Goubert <goubert.clement@gmail.com>
2026-02-02 13:46:16 -0700

    TSD prep all SV and blast - won't report

--- bin/prepTSD.sh
+++ bin/prepTSD.sh
@@ -8,6 +8,7 @@ REF=$1 # ref genome
 WIN=$2 # windows size in flanking to search TSD
 MSK="indels.fa.masked" # masked SVs from repeatmasker
 OUT_VCF="pangenie.vcf"
+FASTA_FILE=indels.fa
 
 ###################################################
 # Step 1: extract flanking of each retained TE SV #
@@ -18,27 +19,37 @@ echo "extracting flanking..."
 grep "contig=" ${VCF} | sed 's/\#\#contig=<ID=//g;s/,length=/\t/g;s/>//g' > gLength.txt
 
 # create a bed with vcf entries
-bcftools view -H ${VCF} | \
- grep 'n_hits=1;\|n_hits=2;' | \
- grep -v 'mam_filter_2=VNTR_ONLY' | \
- awk '/n_hits=1/ && length($4) < length($5) {print $1"\t"$2"\t"($2)+1"\t"$3; next} /n_hits=1/ && length($4) > length($5) {print $1"\t"$2"\t"($2+length($4))"\t"$3; next} /n_hits=2/ && length($4) < length($5) && /5P_INV/ {print $1"\t"$2"\t"($2)+1"\t"$3; next} /n_hits=2/ && length($4) > length($5) && /5P_INV/ {print $1"\t"$2"\t"($2+length($4))"\t"$3}' > oneHit_SV_coordinates.bed
+# bcftools view -H ${VCF} | \
+#  grep 'n_hits=1;\|n_hits=2;' | \
+#  grep -v 'mam_filter_2=VNTR_ONLY' | \
+#  awk '/n_hits=1/ && length($4) < length($5) {print $1"\t"$2"\t"($2)+1"\t"$3; next} /n_hits=1/ && length($4) > length($5) {print $1"\t"$2"\t"($2+length($4))"\t"$3; next} /n_hits=2/ && length($4) < length($5) && /5P_INV/ {print $1"\t"$2"\t"($2)+1"\t"$3; next} /n_hits=2/ && length($4) > length($5) && /5P_INV/ {print $1"\t"$2"\t"($2+length($4))"\t"$3}' > oneHit_SV_coordinates.bed
+
+# now we simply extract all SV
+bcftools view -H ${VCF} |  awk '{ if(length($4) < length($5)) {print $1"\t"$2"\t"($2)+1"\t"$3} if (length($4) > length($5)) {print $1"\t"$2"\t"($2+length($4))"\t"$3}}' > oneHit_SV_coordinates.bed
+
 
 # extend +/- ${WIN} bp in two entries per SV
-cat <(bedtools slop -i oneHit_SV_coordinates.bed -g gLength.txt -l 30 -r 0 | awk '{print $0"__L"}') \
-<(bedtools slop -i oneHit_SV_coordinates.bed -g gLength.txt -l 0 -r 30 | awk '{print $0"__R"}') | \
-sort -k1,1 -k2,2n -k3,3n | awk '/__L/ {print $1":"$2"-"($2+30); next} /__R/ {print $1":"($3-30)"-"$3}' > oneHit_SV_coordinates_win.regions
+cat <(bedtools slop -i oneHit_SV_coordinates.bed -g gLength.txt -l ${WIN} -r 0 | awk '{print $0"__L"}') \
+<(bedtools slop -i oneHit_SV_coordinates.bed -g gLength.txt -l 0 -r ${WIN} | awk '{print $0"__R"}') | \
+sort -k1,1 -k2,2n -k3,3n | awk -v win=${WIN} '/__L/ {print $1":"$2"-"($2+win); next} /__R/ {print $1":"($3-win)"-"$3}' > SV_coordinates_win.regions
 # extract fasta from flanking
-samtools faidx -r oneHit_SV_coordinates_win.regions -o flanking_sequences.fasta ${REF}
+samtools faidx -r SV_coordinates_win.regions -o flanking_sequences.fasta ${REF}
 
 ##################################################
 # Step 2: extract 5' and 3' of each masked TE SV #
 ##################################################
 echo "extracting SVs' 5' and 3' ends..."
 
-# filter the indels.fa.masked to keep only single RM hits (1 TE per SV)
-perl -ne 'if(/^>(\S+)/){$c=$i{$1}}$c?print:chomp;$i{$_}=1 if @ARGV' <(cut -f 4 oneHit_SV_coordinates.bed) ${MSK} > oneHit_indels.fa.masked
+# we don't need that anymore
+# # filter the indels.fa.masked to keep only single RM hits (1 TE per SV)
+# perl -ne 'if(/^>(\S+)/){$c=$i{$1}}$c?print:chomp;$i{$_}=1 if @ARGV' <(cut -f 4 SV_coordinates.bed) ${MSK} > oneHit_indels.fa.masked
+
+# new: make an indel.fa file:
+bcftools view -H --types indels --include 'ILEN>0' ${VCF} | awk '{print(sprintf(">%s\n%s", $3, $5))}' >> ${FASTA_FILE}
+bcftools view -H --types indels --include 'ILEN<0' ${VCF} | awk '{print(sprintf(">%s\n%s", $3, $4))}' >> ${FASTA_FILE}
+
 # linearize fasta, then trim and split in two seq (L and R)
-awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' oneHit_indels.fa.masked | \
+awk '/^>/ {printf("%s%s\t",(N>0?"\n":""),$0);N++;next;} {printf("%s",$0);} END {printf("\n");}' indels.fa | \
 awk -v len=${WIN} -F '\t' '{x=len;L=length($2);printf("%s\n%s\n%s\n%s\n",$1"__L",(L<=x?$2:substr($2,2,x+1)),$1"__R",(L<=x?$2:substr($2,1+L-x,x)));}' > SV_sequences_L_R_trimmed_WIN.fa
 # export the list of SV to search TSD for next process parallelization
 grep '>' SV_sequences_L_R_trimmed_WIN.fa | sed 's/>//g;s/__/\t/g' | cut -f 1 | sort | uniq > indels.txt
