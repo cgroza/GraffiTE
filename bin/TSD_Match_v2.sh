5772528efeb23fc8567a7eb8a1c21bf7e40d4b23
Clement Goubert <goubert.clement@gmail.com>
2026-02-03 14:55:19 -0700

    score by paired distance to breakpoint

--- bin/TSD_Match_v2.sh
+++ bin/TSD_Match_v2.sh
@@ -114,7 +114,7 @@ makeblastdb -in L.fasta -out L.fasta -dbtype="nucl"
 # 	blastn -word_size 4 -query R.short.fasta -db L.short.fasta -outfmt 6 -strand plus | awk -v div={$DIV} '$5+$6 <= 1 || ($5+$6)/$4 <= div/100' | sort -k7,7n -k10,10nr -k4,4nr > blastout 2>&1
 
 # now we blast no matter what and I will just save the table for now
-exact_match.py -word_size 4 -query R.fasta -db L.fasta -outfmt 6 -strand plus | awk 'function abs(x) { return x < 0 ? -x : x } {print $0"\t"(30-($7+$8)/2)"\t"(30-($9+$10)/2)"\t"abs(30-($7+$8)/2)"\t"abs(30-($9+$10)/2)}' > blastout 2>&1
+exact_match.py -word_size 4 -query R.fasta -db L.fasta -outfmt 6 -strand plus | awk 'function abs(x) { return x < 0 ? -x : x } function min(x,y) { return x < y ? x : y } { a = (abs(30-$7) + abs(30-$8)) / 2; b = (abs(30-$9) + abs(30-$10)) / 2; score = min(a, b); print $0"\t"abs(30-$7)"\t"abs(30-$8)"\t"abs(30-$9)"\t"abs(30-$10)"\t"score }' > blastout 2>&1
 	
 # 	if ! [[ -s blastout ]]
 # 	then
