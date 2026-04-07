c7b40568b60a6309ac6ce355e69a6a9bafdc309f
Cristian Groza <cristian.groza@mail.mcgill.ca>
2026-04-07 15:30:17 -0400

    fix(subset_gaf): set MAPQ field to 1 as temporary workaround for vg bug

--- bin/subset_gaf.py
+++ bin/subset_gaf.py
@@ -10,6 +10,8 @@ for line in sys.stdin:
             cs = f
             break
 
+    # temporary work around (vg convert GAM to GAF sets MAPQ=0)
+    fields[11] = '1'
     basic_fields ='\t'.join(fields[:12])
     if cs is not None:
         print(basic_fields + "\t" + cs)
